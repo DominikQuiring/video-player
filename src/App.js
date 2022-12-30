e1d1227c8d920e0ee85d8b264ad8d3262e220814
DominikQuiring <dominikquiring@gmail.com>
2022-12-30 11:08:13 -0700

    mediaplayer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ReactPlayer controls url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
+    </div>
+  );
+}
+
+export default App;
